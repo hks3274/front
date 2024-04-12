db904a08db28f7b0d3a03061b744c105fd084d29
sooYeon <hks3274@naver.com>
2024-04-12 09:31:24 +0900

    20240411

---
+++ project/JS/tetris.js
@@ -0,0 +1,58 @@
+
+//DOM
+const palyground = document.querySelector(".playground > ul");
+
+//setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
+
+//variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BOLOKS = {
+    tree: [
+        [[0,0],[0,1],[1,0],[1,1]],
+        [],
+        [],
+        [],
+    ]
+}
+
+
+const movingIntem = {
+    type : "tree",
+    direction: 0,
+    top: 0,
+    left: 0,
+    
+}
+
+init()
+
+//functions 
+function init() {
+    tempMovingItem = {...movingIntem}; //이 안의 값만 가져옴 spread 기존의 객체를 건들지 않는다.
+    for (let i = 0; i < GAME_ROWS; i++) {
+        prependNewLine()
+    }
+}
+
+
+
+function prependNewLine() {
+    const li = document.createElement("li");
+    const ul = document.createElement("ul");
+    for (let j = 0; j < 10; j++) {
+        const matrix = document.createElement("li");
+        ul.prepend(matrix);
+    }
+    li.prepend(ul);
+    palyground.prepend(li);
+}
+
+function renderBlocks(){
+
+}
